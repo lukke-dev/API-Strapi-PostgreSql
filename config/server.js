a2ee5a6d4f5a4ff40a4f8fe5e0b8f921a2358837
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-07-17 14:26:01 -0300

    ini

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6f01c8ac137df48c2133d91fdf360644'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b4fd6e25-c7de-493c-9953-a9f2bdd588cc'
+};
